dd8a4d29ed33c791f39bfe228f2cd06698da470f
ljrgov <129682266+ljrgov@users.noreply.github.com>
2026-08-07 22:40:01 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# sed -i "s/$(grep -oP 'version=\K[^ ]+' module.prop)/$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}')($(git log --oneline -n 1 | awk '{print $1}'))/g" module.prop
+zip -r -o -X -ll crond4Android-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'LICENSE'
